060aea9226074fcd2923e62123a479588961de08
Emre ALŞAN <97472215+emrealsandev@users.noreply.github.com>
2023-03-10 23:15:23 +0300

    Change data output

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
 
 /**
  * SiteController
@@ -22,8 +23,12 @@ class SiteController extends Controller
     {
         return $this->render('contact');
     }
-    public function handleContact()
+    public function handleContact(Request $request)
     {
-        return "emre";
+        $body = $request->getBody();
+        echo '<pre>';
+        var_dump($body);
+        echo '</pre>';
+        exit;
     }
 }
