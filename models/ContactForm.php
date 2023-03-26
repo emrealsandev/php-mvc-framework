f26abc1d540fb1c875d71e45f539b5c75b2dcbc7
Emre ALŞAN <97472215+emrealsandev@users.noreply.github.com>
2023-03-26 13:19:52 +0300

    Contact Form

---
+++ models/ContactForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class ContactForm extends Model
+{
+    public string $subject = '';
+    public string $email = '';
+    public string $body = '';
+
+    public function rules(): array
+    {
+        return [
+            'subject' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED],
+            'body' => [self::RULE_REQUIRED],
+        ];
+    }
+
+    public function labels(): array     
+    {
+        return [
+            'subject' => 'Enter Your Subject',
+            'email' => 'Your email',
+            'body' => 'Body',
+        ];
+    }
+
+}
--- views/contact.php
+++ views/contact.php
@@ -1,21 +1,16 @@
 <?php 
 /** @var $this \app\core\View */
+/** @var $model \app\models\ContactForm */
+
+use app\core\form\Form;
+
 $this->title = 'Contact';
 ?>
 
 <h1>contact us</h1>
-    <form action="" method="post">
-        <div class="form-group">
-            <label>Subject</label>
-            <input type="text" name="subject" class="form-control">
-        </div>
-        <div class="form-group">
-            <label>email</label>
-            <input type="text" name="email" class="form-control">
-        </div>
-        <div class="form-group">
-            <label>body</label>
-            <textarea name="body" class="form-control"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary mt-2">Submit</button>
-    </form>
+<?php $form = Form::begin('','post') ?>
+<?php echo $form->field($model,'subject')?>
+<?php echo $form->field($model,'email')?>
+<?php echo $form->field($model,'body')?>
+<button type="submit" class="btn btn-primary mt-2">Submit</button>
+<?php Form::end(); ?>
