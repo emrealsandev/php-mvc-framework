03c4634249cc63c60268cf5d3663b5d6031215a6
Emre ALŞAN <97472215+emrealsandev@users.noreply.github.com>
2023-03-27 21:17:38 +0300

    textarea form element in class

--- views/contact.php
+++ views/contact.php
@@ -3,6 +3,7 @@
 /** @var $model \app\models\ContactForm */
 
 use app\core\form\Form;
+use app\core\form\TextareaField;
 
 $this->title = 'Contact';
 ?>
@@ -11,6 +12,6 @@ $this->title = 'Contact';
 <?php $form = Form::begin('','post') ?>
 <?php echo $form->field($model,'subject')?>
 <?php echo $form->field($model,'email')?>
-<?php echo $form->field($model,'body')?>
+<?php echo new TextareaField($model,'body') ?>
 <button type="submit" class="btn btn-primary mt-2">Submit</button>
 <?php Form::end(); ?>
